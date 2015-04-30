adc6aa8dfc6d67055fdb46668dcc28a311aca58f
Elena Volokhina <elenav@indigital.net>
2015-04-29 21:03:59 -0400

    created QuickSort JUnit test

--- src/main/java/algorithms/QuickSort.java
+++ src/main/java/algorithms/QuickSort.java
@@ -1,5 +1,11 @@
 package algorithms;
 
+import java.util.List;
+
 public class QuickSort {
 
+	public List<Integer> quickSort(List<Integer> list) {
+		// TODO
+		return null;
+	}
 }
--- src/test/java/algorithms/QuickSortTest.java
+++ src/test/java/algorithms/QuickSortTest.java
@@ -1,5 +1,44 @@
 package algorithms;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.apache.log4j.Logger;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class QuickSortTest {
 
+	private final static Logger logger = Logger.getLogger(QuickSortTest.class);
+
+	private static List<Integer> expectedList;
+	private static List<Integer> shuffledList;
+
+	@BeforeClass
+	public static void setUpBeforeClass() {
+
+		expectedList = new ArrayList<Integer>(Arrays.asList(-5, -1, 0, 2, 2, 5, 7, 100));
+		
+		shuffledList = new ArrayList<Integer>(expectedList);
+		Collections.shuffle(shuffledList);
+		
+		logger.info("Expected list: " + expectedList);
+		logger.info("Shuffled list: " + shuffledList);
+	}
+
+	@Test
+	public final void testQuickSort() {
+
+			QuickSort sorter = new QuickSort();
+			
+			List<Integer> sortedList = sorter.quickSort(shuffledList);			
+			logger.info("Sorted list: " + sortedList);
+			
+			assertArrayEquals("quickSort works", expectedList.toArray(), sortedList.toArray());
+	}
 }
