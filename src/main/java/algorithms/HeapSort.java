01f7a87c2449345206d1261a504f680e63d25a22
Elena Volokhina <moodcheerful@users.noreply.github.com>
2015-05-09 23:31:45 -0400

    created HeapSort Junit test

---
+++ src/main/java/algorithms/HeapSort.java
@@ -0,0 +1,14 @@
+package algorithms;
+
+public class HeapSort {
+
+	// HeapSort algorithm
+	// Time complexity is O(n*log(n))
+
+	@SuppressWarnings("rawtypes")
+	public static void heapSort(Comparable[] a) {
+		
+		//TODO
+	}
+	
+}
