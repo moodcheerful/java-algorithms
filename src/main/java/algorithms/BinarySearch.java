cb79c23418e31fb4426f4ea4a34f29de5b5372b4
Elena Volokhina <moodcheerful@users.noreply.github.com>
2015-05-18 17:06:26 -0400

    implemented BinarySearch

--- src/main/java/algorithms/BinarySearch.java
+++ src/main/java/algorithms/BinarySearch.java
@@ -6,12 +6,33 @@ public class BinarySearch {
 
 	// BinarySearch algorithm of searching for an element in a SORTED list
 	// Time complexity is O(log n)
-	
+
 	// List<T> list must be sorted in ascending order
 	public static <T extends Comparable<T>> int binarySearch(List<T> list, T element) {
+
+		int low = 0;
 		
-		//TODO
+		int high = list.size() - 1;
+
+		while (low <= high) {
+			
+			int mid = low + (high - low) / 2;
+			
+			int comp = element.compareTo(list.get(mid));
+			
+			if (comp < 0) {
+				
+				high = mid - 1;
+				
+			} else if (comp > 0) {
+				
+				low = mid + 1;
+				
+			} else {
+				// element found:
+				return mid;
+			}
+		}
 		return -1;
 	}
-
 }
