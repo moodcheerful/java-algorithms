e1c6d8e38318b62289cfe2e1499acbf6e33b6608
Elena Volokhina <moodcheerful@users.noreply.github.com>
2015-10-24 14:58:52 -0400

    cleaned up indentation formatting

--- src/main/java/algorithms/MaintainingMedian.java
+++ src/main/java/algorithms/MaintainingMedian.java
@@ -6,20 +6,21 @@ import java.util.Comparator;
 
 /**
  * Median Maintenance Algorithm.
- *  
- * Given a sequence	{x1, ..., xn} of numbers one-by-one from a data stream,
- * find at each step i the median of {x1, ..., xi}.
+ * 
+ * Given a sequence {x1, ..., xn} of numbers one-by-one from a data stream, find at each step i the
+ * median of {x1, ..., xi}.
  * 
  * Time Complexity: O(log(i)) at each step i.
  * 
- * Solution: maintain 2 heaps, one that supports Extract-Min operation and one that supports Extract-Max,
- * and maintain invariant that maxHeap on the left half holds elements that are less than the effective median, 
- * and minHeap on the right half holds elements that are greater than the effective median.
+ * Solution: maintain 2 heaps, one that supports Extract-Min operation and one that supports
+ * Extract-Max, and maintain invariant that maxHeap on the left half holds elements that are less
+ * than the effective median, and minHeap on the right half holds elements that are greater than the
+ * effective median.
  * 
  */
 public class MaintainingMedian {
 
-	private double median;			// rolling median of the data stream
+	private double median; // rolling median of the data stream
 	private Queue<Integer> minHeap; // right half (elements >= median)
 	private Queue<Integer> maxHeap; // left half (elements <= median)
 
@@ -43,49 +44,49 @@ public class MaintainingMedian {
 	public void addNumber(int x) {
 
 		// initialize
-		if (minHeap.isEmpty()) { 
-			
+		if (minHeap.isEmpty()) {
+
 			// add first element:
 			if (maxHeap.isEmpty()) {
 				maxHeap.offer(x);
 				median = x;
-			
+
 			// add second element:
 			} else {
 				minHeap.offer(x);
 				median = (double) (minHeap.peek() + maxHeap.peek()) / 2;
 			}
-		
+
 		// process the rest of the elements:
 		} else {
 
 			// step 1: add next element to one of the heaps:
-			if (x < maxHeap.peek()) {	// O(1)
-				maxHeap.offer(x);		// O(log(i))
-			} else {				
-				minHeap.offer(x);		// O(log(i))
+			if (x < maxHeap.peek()) { // O(1)
+				maxHeap.offer(x); // O(log(i))
+			} else {
+				minHeap.offer(x); // O(log(i))
 			}
 
-			// step 2: rebalance the heaps:		
-			if (maxHeap.size() - minHeap.size() > 1) {	// O(1)
-				
-				Integer extra = maxHeap.poll();			// O(log(i))
-				minHeap.offer(extra);					// O(log(i))
-				
+			// step 2: rebalance the heaps:
+			if (maxHeap.size() - minHeap.size() > 1) { // O(1)
+
+				Integer extra = maxHeap.poll(); // O(log(i))
+				minHeap.offer(extra); // O(log(i))
+
 			} else if (minHeap.size() - maxHeap.size() > 1) {
-				
-				Integer extra = minHeap.poll();			// O(log(i))
-				maxHeap.offer(extra);					// O(log(i))
+
+				Integer extra = minHeap.poll(); // O(log(i))
+				maxHeap.offer(extra); // O(log(i))
 			}
-			
+
 			// step 3: recalculate median:
-			if (minHeap.size() == maxHeap.size()) {							// O(1)	
-				median = (double) (minHeap.peek() + maxHeap.peek()) / 2;	// O(1)	
-			} else if (minHeap.size() > maxHeap.size()) {				
-				median = minHeap.peek();									// O(1)
-			} else {				
-				median = maxHeap.peek();									// O(1)
-			}		
+			if (minHeap.size() == maxHeap.size()) { // O(1)
+				median = (double) (minHeap.peek() + maxHeap.peek()) / 2; // O(1)
+			} else if (minHeap.size() > maxHeap.size()) {
+				median = minHeap.peek(); // O(1)
+			} else {
+				median = maxHeap.peek(); // O(1)
+			}
 		}
 	}
 
