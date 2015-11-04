bea43755047457a73040f87719527b3f09ebc747
Elena Volokhina <moodcheerful@gmail.com>
2015-11-04 17:48:56 -0500

    implemented binary to decimal conversion algorithm

--- src/main/java/algorithms/DecimalToBinary.java
+++ src/main/java/algorithms/DecimalToBinary.java
@@ -3,8 +3,13 @@ package algorithms;
 public class DecimalToBinary {
 
 	/*
-	 * Algorithm for converting both positive and negative integers from decimal
-	 * reprisentation to binary. Constant time complexity.
+	 * The Divide by 2 algorithm for converting integers (both positive and negative) 
+	 * from decimal to binary. Constant time complexity.
+	 * 
+	 * For decimal n represented as abcd in base 2:
+	 * n = a*2^3 + b*2^2 + c*2^1 + d*2^0 = 2*(a*2^2 + b*2^1 + c*2^0) + d = 
+	 * = 2 * floor(n / 2) + (n % 2)
+	 * 
 	 */
 	public static String decimalToBinary(int decimal) {
 
@@ -24,5 +29,27 @@ public class DecimalToBinary {
 			}
 		}
 		return binary;
+	}
+
+	/*
+	 * Algorithm for converting binary to decimal integers (both positive and negative).
+	 * Constant time complexity.
+	 * 
+	 * 11011001 = (1 * 2^7) + (1 * 2^6) + (1 * 2^4) + (1 * 2^3) + (1 * 2^0) = 
+	 * 			=	1 << 7 | 1 << 6 | 1 << 4 | 1 << 3 | 1 << 0
+	 */
+	public static int binaryToDecimal(String binary) {
+
+		int decimal = 0;
+		int n = binary.length(); // n <= 32
+
+		for (int i = 0; i < n; i++) {
+
+			if (binary.charAt(n - i - 1) == '1') {
+
+				decimal = decimal | (1 << i);
+			}
+		}
+		return decimal;
 	}
 }
--- src/test/java/algorithms/DecimalToBinaryTest.java
+++ src/test/java/algorithms/DecimalToBinaryTest.java
@@ -8,22 +8,43 @@ public class DecimalToBinaryTest {
 
 	private static Logger logger = Logger.getLogger(DecimalToBinaryTest.class);
 
+	private static int[] decimals;
+
+	@BeforeClass
+	public static void setupBeforeClass() {
+
+		decimals = new int[] {Integer.MIN_VALUE, Integer.MIN_VALUE + 1, -3, -2,
+				-1, 0, 1, 2, 3, Integer.MAX_VALUE - 1, Integer.MAX_VALUE};
+	}
+
 	@Test
 	public void testDecimalToBinary() {
 
-		int[] numbers = { Integer.MIN_VALUE, Integer.MIN_VALUE + 1, -2, -1, 0,
-				1, 2, Integer.MAX_VALUE - 1, Integer.MAX_VALUE };
+		for (int i = 0; i < decimals.length; i++) {
 
-		for (int i = 0; i < numbers.length; i++) {
+			String expected = Integer.toBinaryString(decimals[i]);
 
-			String expected = Integer.toBinaryString(numbers[i]);
+			String result = DecimalToBinary.decimalToBinary(decimals[i]);
 
-			String result = DecimalToBinary.decimalToBinary(numbers[i]);
-
-			logger.info("number: " + numbers[i] + ", binary representation: "
-					+ result);
+			logger.info("decimal: " + decimals[i] + ", binary: " + result);
 
 			Assert.assertTrue(expected.equals(result));
+		}
+	}
+
+	@Test
+	public void testBinaryToDecimal() {
+
+		for (int i = 0; i < decimals.length; i++) {
+
+			int expected = decimals[i];
+
+			int result = DecimalToBinary.binaryToDecimal(Integer.toBinaryString(expected));
+
+			logger.info("binary: " + Integer.toBinaryString(expected)
+					+ ", decimal: " + result);
+
+			Assert.assertEquals(expected, result);
 		}
 	}
 }
